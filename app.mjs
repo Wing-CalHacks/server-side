065dd142e44caf8f9a1bcb33790e02c4979feaa5
Shrinandan Narayanan <emailshri.n@gmail.com>
2023-06-18 01:38:46 -0700

    add analytics

--- app.mjs
+++ app.mjs
@@ -1,5 +1,20 @@
-import express from "express";
-import { getWebsites, addWebsite, addUser, getUsers } from "./db.mjs";
+import express, { json, response } from "express";
+import {
+  getWebsites,
+  addWebsite,
+  addUser,
+  getUsers,
+  getSessions,
+  addSession,
+  addConversation,
+  getConversation,
+  getProduct,
+  addProduct,
+  getUserProducts,
+  addUserProducts,
+  generateSQLInsertStatements,
+  executeForecasting,
+} from "./db.mjs";
 const app = express();
 app.use(express.json()); // To parse JSON request bodies
 app.use(express.urlencoded({ extended: true })); // To parse URL-encoded request bodies
@@ -18,6 +33,8 @@ app.post("/websites", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
+// users
 app.post("/users", async (req, res) => {
   const { website_id, user_name, user_email } = req.body;
   try {
@@ -27,12 +44,119 @@ app.post("/users", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
-
 app.get("/users", async (req, res) => {
   const { website_id } = req.body;
   try {
     const users = await getUsers(website_id);
     res.status(200).send(users);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//sessions
+app.post("/sessions", async (req, res) => {
+  const { user_id, start_time, end_time } = req.body;
+  try {
+    await addSession(user_id, start_time, end_time);
+    res.status(200).send("session added successfully");
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+app.get("/sessions", async (req, res) => {
+  const { user_id } = req.body;
+  try {
+    const session = await getSessions(user_id);
+    res.status(200).send(session);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+//conversations
+app.post("/conversations", async (req, res) => {
+  const { session_id, query, response, rating, click_links, added_products } =
+    req.body;
+  try {
+    await addConversation(
+      session_id,
+      query,
+      response,
+      rating,
+      click_links,
+      added_products
+    );
+    res.status(200).send("conversation added successfully");
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+app.get("/conversations", async (req, res) => {
+  const { session_id } = req.body;
+  try {
+    const conversation = await getConversation(session_id);
+    res.status(200).send(conversation);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+// products
+app.post("/products", async (req, res) => {
+  const { name, color, link, material, description, sleeves, price } = req.body;
+  try {
+    await addProduct(name, color, link, material, description, sleeves, price);
+    res.status(200).send("product added successfully");
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+app.get("/products", async (req, res) => {
+  const { product_id } = req.body;
+  try {
+    const product = await getProduct(product_id);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.get("/analytics", async (req, res) => {
+  try {
+    const inputData = await getUserProducts();
+    // var userAnalytics = new Map();
+    // var productAnalytics = new Map();
+    const transformedData = {};
+
+    inputData.forEach(({ product_id, date }) => {
+      const formattedDate = new Date(date).toISOString().split("T")[0];
+
+      if (transformedData[formattedDate]) {
+        if (transformedData[formattedDate][product_id]) {
+          transformedData[formattedDate][product_id]++;
+        } else {
+          transformedData[formattedDate][product_id] = 1;
+        }
+      } else {
+        transformedData[formattedDate] = { [product_id]: 1 };
+      }
+    });
+
+    const statements = generateSQLInsertStatements(transformedData);
+    await executeForecasting(statements);
+
+    res.status(200).send("analytics added successfully");
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/analytics", async (req, res) => {
+  const { user_id, product_id } = req.body;
+  try {
+    await addUserProducts(user_id, product_id);
+    res.status(200).send("analytics added successfully");
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
--- db.mjs
+++ db.mjs
@@ -50,3 +50,103 @@ export async function getUsers(website_id) {
 }
 
 //session
+export async function addSession(user_id, start_time, end_time) {
+  const result = await pool.query(
+    "INSERT INTO Sessions (user_id, start_time, end_time) VALUES (?, ?, ?)",
+    [user_id, start_time, end_time]
+  );
+  return result;
+}
+
+export async function getSessions(user_id) {
+  const [rows] = await pool.query(
+    "SELECT Sessions.session_id, Sessions.start_time, Sessions.end_time FROM Sessions JOIN Users ON Sessions.user_id = Users.user_id WHERE Users.user_id = ?;",
+    [user_id]
+  );
+  return rows;
+}
+
+//conversation
+export async function addConversation(
+  session_id,
+  query,
+  response,
+  rating,
+  click_links,
+  added_products
+) {
+  const result = await pool.query(
+    "INSERT INTO Conversations (session_id, query, response, rating, click_links, added_products) VALUES (?, ?, ?, ?, ?, ?)",
+    [session_id, query, response, rating, click_links, added_products]
+  );
+  return result;
+}
+
+export async function getConversation(session_id) {
+  const [rows] = await pool.query(
+    "SELECT * FROM Conversations WHERE session_id = ?;",
+    [session_id]
+  );
+  return rows;
+}
+
+export async function addProduct(
+  name,
+  color,
+  link,
+  material,
+  description,
+  sleeves,
+  price
+) {
+  const result = await pool.query(
+    "INSERT INTO products (name, color, link, material, description, sleeves, price) VALUES (?, ?, ?, ?, ?, ?, ?)",
+    [name, color, link, material, description, sleeves, price]
+  );
+  return result;
+}
+
+export async function getProduct(product_id) {
+  const [rows] = await pool.query("SELECT * FROM products WHERE id = ?;", [
+    product_id,
+  ]);
+  return rows;
+}
+
+export async function getUserProducts() {
+  const [rows] = await pool.query("SELECT * FROM UserProducts;");
+  return rows;
+}
+
+export async function addUserProducts(user_id, product_id, date) {
+  const result = await pool.query(
+    "INSERT INTO UserProducts (user_id, product_id, date) VALUES (?, ?)",
+    [user_id, product_id, date]
+  );
+
+  return result;
+}
+
+export function generateSQLInsertStatements(transformedData) {
+  const sqlStatements = [];
+
+  Object.entries(transformedData).forEach(([date, products]) => {
+    Object.entries(products).forEach(([product_id, clicks]) => {
+      const sql = `INSERT INTO forecast (date, product_id, clicks) VALUES ('${date}', ${product_id}, ${clicks});`;
+      sqlStatements.push(sql);
+    });
+  });
+
+  return sqlStatements;
+}
+
+export async function executeForecasting(sqlStatements) {
+  for (const sql of sqlStatements) {
+    try {
+      await pool.query(sql);
+      console.log(`Successfully executed SQL: ${sql}`);
+    } catch (error) {
+      console.error(`Error executing SQL: ${sql}`, error);
+    }
+  }
+}
